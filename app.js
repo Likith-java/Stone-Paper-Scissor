51ffd3e1bb84c43548e20a572e90ec9a1e3520df
Likith V Shetty <likithvshetty241279@gmail.com>
2025-09-09 22:15:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let userScore = 0;
+let compScore = 0;
+
+let choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper","scissors"];
+    // rock,paper,scissors
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+};
+
+const drawGame = () =>{
+    // console.log("Game was draw.");
+    msg.innerText = "Game was Draw, Play again";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin === true){
+        userScore++;
+        userScorePara.innerText = userScore;
+        // console.log("You Win!!!");
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        // console.log("You lose...");
+        msg.innerText = `You lose. ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) =>{
+    console.log("User choice = ",userChoice);
+    // Generate computer choice -> modular approach
+    const compChoice = genCompChoice();
+    console.log("Comp choice = ",compChoice);
+
+    if(userChoice ===  compChoice){
+        // Draw Game
+        drawGame(); 
+    }
+    else{
+        let userWin =  true;
+        if(userChoice === "rock"){
+            // scissors/paper
+            userWin = compChoice === "paper" ? false : true;
+        }else if(userChoice === "paper"){
+            //rock/scissors
+            userWin = compChoice === "scissors" ? false : true;
+        }else{
+            //rock/paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    // console.log(choice);
+    choice.addEventListener("click",() =>{
+        const userChoice = choice.getAttribute("id");
+        // console.log("Choice was clicked",userChoice);
+        playGame(userChoice);
+    });
+});
